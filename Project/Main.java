e33fcd1483e827e3d03e44c41bdbb3c0c8495439
VV01T3K <wojteks.access@gmail.com>
2024-02-25 13:39:51 +0000

    example files

---
+++ Project/Main.java
@@ -0,0 +1,10 @@
+package Project;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("John Doe");
+        System.out.println("\nHello, " + person.getName());
+        System.out.println("\nHello, " + person.getName());
+        System.out.println("\nHello, " + person.getName());
+    }
+}
