6f528e9a570f139d1c44f2ab728b49b1cb274ce9
VV01T3K <wojteks.access@gmail.com>
2024-03-17 21:30:21 +0000

    clean defalut

--- pl/edu/pg/eti/ksg/po/lab1/transformacje/Transformacja.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pg.eti.ksg.po.lab1.transformacje;
-
-public interface Transformacja {
-    Punkt transformuj(Punkt p);
-
-    Transformacja getTransformacjaOdwrotna()
-            throws BrakTransformacjiOdwrotnejException;
-}
