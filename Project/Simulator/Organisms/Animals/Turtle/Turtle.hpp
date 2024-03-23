140a9857093b9d62e3319dbf8062a3c68442a96b
VV01T3K <wojteks.access@gmail.com>
2024-03-23 01:01:26 +0000

    bad turtle

---
+++ Project/Simulator/Organisms/Animals/Turtle/Turtle.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <typeinfo>
+
+#include "../Animal.hpp"
+class Turtle : public Animal {
+   public:
+    Turtle(int x, int y, World &world) : Animal(2, 1, x, y, world) {}
+    Turtle(Position position, World &world) : Animal(2, 1, position, world) {}
+
+    void action() override {
+        if (rng.roll(0, 3) == 0) Animal::action();
+    }
+
+    void collision(Organism &other) override {
+        if (typeid(other) == typeid(Animal)) {
+            if (other.getPower() < 5) {
+                Animal::collision(other);
+                static_cast<Animal &>(other).undoMove();
+            }
+        }
+    }
+};
