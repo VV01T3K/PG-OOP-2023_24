531121493ff2cd256587e073d5389033211cfebf
VV01T3K <wojteks.access@gmail.com>
2024-03-23 01:12:20 +0000

    antylopa

--- Project/Simulator/Organisms/Animals/Antelope/Antelope.hpp
+++ Project/Simulator/Organisms/Animals/Antelope/Antelope.hpp
@@ -3,16 +3,35 @@
 #include "../Animal.hpp"
 
 class Antelope : public Animal {
+   private:
+    Position oldestPosition;
+
    public:
-    Antelope(int x, int y, World &world) : Animal(4, 4, x, y, world) {}
-    Antelope(Position position, World &world) : Animal(4, 4, position, world) {}
+    Antelope(int x, int y, World &world)
+        : Animal(4, 4, x, y, world), oldestPosition(x, y) {}
+
+    Antelope(Position position, World &world)
+        : Animal(4, 4, position, world), oldestPosition(position) {}
 
     void action() override {
-        oldPosition = position;
+        oldestPosition = oldPosition = position;
+        // first step
         while (oldPosition == position) {
             move(static_cast<Direction>(
                 rng.roll(0, 3)));  // 0-UP, 1-DOWN, 2-LEFT, 3-RIGHT
         }
+        // second step
+        oldPosition = position;
+        while (oldPosition == position && oldestPosition == position) {
+            move(static_cast<Direction>(
+                rng.roll(0, 3)));  // 0-UP, 1-DOWN, 2-LEFT, 3-RIGHT
+        }
+    }
+    void undoMove() override {
+        if (position == oldPosition) {
+            position = oldestPosition;
+        } else
+            position = oldPosition;
     }
     void collision(Organism &other) override {
         if (other.getPower() > power) {
