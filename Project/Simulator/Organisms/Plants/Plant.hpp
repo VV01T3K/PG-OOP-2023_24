2babfd9fe4b25268a4960515281fa7b925695ff4
VV01T3K <wojteks.access@gmail.com>
2024-03-23 01:28:48 +0000

    plantplant

--- Project/Simulator/Organisms/Plants/Plant.hpp
+++ Project/Simulator/Organisms/Plants/Plant.hpp
@@ -11,14 +11,33 @@ class Plant : public Organism {
     RandGen& rng;
 
    public:
-    Plant(int power, int initiative, int x, int y, World& world)
-        : Organism(power, initiative, x, y, world),
-          rng(RandGen::getInstance()) {}
-    Plant(int power, int initiative, Position position, World& world)
-        : Organism(power, initiative, position, world),
-          rng(RandGen::getInstance()) {}
+    Plant(int power, int x, int y, World& world)
+        : Organism(power, 0, x, y, world), rng(RandGen::getInstance()) {}
+    Plant(int power, Position position, World& world)
+        : Organism(power, 0, position, world), rng(RandGen::getInstance()) {}
 
-    virtual void action() = 0;
-    virtual void collision(Organism& other) = 0;
-    virtual void draw() = 0;
+    virtual void action() override {
+        if (rng.roll(0, 50) < 5) {
+            Position newPosition = position;
+            while (newPosition == position || world.isOccupied(newPosition)) {
+                newPosition = position;
+                switch (static_cast<Direction>(rng.roll(0, 3))) {
+                    case Direction::UP:
+                        newPosition.y = position.y - 1;
+                        break;
+                    case Direction::DOWN:
+                        newPosition.y = position.y + 1;
+                        break;
+                    case Direction::LEFT:
+                        newPosition.x = position.x - 1;
+                        break;
+                    case Direction::RIGHT:
+                        newPosition.x = position.x + 1;
+                        break;
+                }
+            }
+        }
+    }
+    virtual void collision(Organism& other) {}
+    virtual void draw() override {}
 };
