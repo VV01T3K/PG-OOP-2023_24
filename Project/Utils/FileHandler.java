680ddd879d7992e4b4b1d312933ba6c6781ab77b
VV01T3K <wojteks.access@gmail.com>
2024-05-18 18:53:08 +0000

    json

--- Project/Utils/FileHandler.java
+++ Project/Utils/FileHandler.java
@@ -3,3 +3,21 @@ package Utils;
 public class FileHandler {
 
 }
+
+// import java.io.FileReader;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Paths;
+// import org.json.JSONObject;
+
+// public static void main(String[] args) {
+// try {
+// String content = Files.readString(Paths.get("Project/human.json"));
+// JSONObject jsonObject = new JSONObject(content);
+// System.out.println(jsonObject.get("world"));
+// } catch (IOException e) {
+// throw new RuntimeException(e);
+// }
+// }
+
+// import org.json.JSONArray;
