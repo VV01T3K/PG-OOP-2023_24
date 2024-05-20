7732c55b155df68e255062130ea81bbf516011bc
VV01T3K <wojteks.access@gmail.com>
2024-05-20 02:13:08 +0200

    cleanup

--- CPP_Project_to_port_to_java/Simulator/Organisms/Animals/@[AnimalPack].hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Antelope.hpp"
-#include "CyberSheep.hpp"
-#include "Fox.hpp"
-#include "Human.hpp"
-#include "Sheep.hpp"
-#include "Turtle.hpp"
-#include "Wolf.hpp"
--- CPP_Project_to_port_to_java/Simulator/Organisms/Animals/Wolf.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "@Animal.hpp"
-class Wolf : public Animal {
-   public:
-    Wolf(World& world) : Animal(9, 5, world, Type::WOLF) {}
-    Wolf(nlohmann::json json, World& world) : Animal(json, world) {}
-    Animal* construct() const override { return new Wolf(world); }
-};
