92410b636a448dc9cebd838265e4e9ae1de3b246
VV01T3K <wojteks.access@gmail.com>
2024-06-03 19:10:31 +0000

    tile

---
+++ Project/Simulator/Tile.py
@@ -0,0 +1,53 @@
+import random
+
+
+class Tile:
+    def __init__(self, index, directionCount):
+        self.index = index
+        self.directions = [None]*directionCount
+        self.organisms = []
+
+    def clear(self):
+        self.organisms.clear()
+
+    def setLink(self, direction, tile):
+        self.directions[direction.ordinal()] = tile
+
+    def getOrganism(self):
+        return self.organisms[0] if len(self.organisms) > 0 else None
+
+    def placeOrganism(self, organism):
+        self.organisms.append(organism)
+
+    def removeOrganism(self, organism):
+        self.organisms.remove(organism)
+
+    def getRandomFreeNeighbour(self):
+        freeNeighbours = [
+            tile for tile in self.directions if tile != None and tile.isFree()]
+        if len(freeNeighbours) == 0:
+            return None
+        return freeNeighbours[random.randint(0, len(freeNeighbours) - 1)]
+
+    def getNeighbour(self, direction):
+        return self.directions[direction.ordinal()]
+
+    def getRandomNeighbour(self):
+        neighbours = [tile for tile in self.directions if tile != None]
+        if len(neighbours) == 0:
+            return None
+        return neighbours[random.randint(0, len(neighbours) - 1)]
+
+    def isFree(self):
+        return len(self.organisms) == 0
+
+    def getNeighbours(self):
+        return self.directions
+
+    def getOccupiedNeighbours(self):
+        occupiedNeighbours = [
+            tile for tile in self.directions if tile != None and not tile.isFree()]
+        return occupiedNeighbours
+
+    def getOrganismCount(self):
+        return len(self.organisms)
