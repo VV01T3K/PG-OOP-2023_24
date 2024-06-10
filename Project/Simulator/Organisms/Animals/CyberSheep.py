56103f378daaba2082b2452ea953905b7b72d73d
VV01T3K <wojteks.access@gmail.com>
2024-06-10 15:03:45 +0200

    oddanie

--- Project/Simulator/Organisms/Animals/CyberSheep.py
+++ Project/Simulator/Organisms/Animals/CyberSheep.py
@@ -9,7 +9,7 @@ from collections import deque
 
 class CyberSheep(Animal):
     def __init__(self, world, json=None):
-        super().__init__(11, 4, world, Type.CYBER_SHEEP)
+        super().__init__(11, 4, world, Type.CYBER_SHEEP, json)
 
     def construct(self):
         return CyberSheep(self._world)
