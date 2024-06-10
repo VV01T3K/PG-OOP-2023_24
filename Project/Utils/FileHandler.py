5521c60dcb2d78858da264411a2f46d4c2bb5b7a
VV01T3K <wojteks.access@gmail.com>
2024-06-10 14:34:11 +0200

    działa xd

--- Project/Utils/FileHandler.py
+++ Project/Utils/FileHandler.py
@@ -27,16 +27,16 @@ class FileHandler:
             "world": json_world
         }
 
-        with open(f"Project/Saves/{name}.json", "w") as file:
+        with open(f"OOP-PG-2024/Project/Saves/{name}.json", "w") as file:
             json.dump(data, file, indent=4)
 
     @staticmethod
     def listSaves():
-        return [os.path.splitext(f.name)[0] for f in sorted(list(filter(lambda f: f.is_file(), list(Path("Project/Saves/").glob("*.json")))))]
+        return [os.path.splitext(f.name)[0] for f in sorted(list(filter(lambda f: f.is_file(), list(Path("OOP-PG-2024/Project/Saves/").glob("*.json")))))]
 
     @staticmethod
     def loadWorld(name: str) -> World:
-        with open(f"Project/Saves/{name}.json", "r") as file:
+        with open(f"OOP-PG-2024/Project/Saves/{name}.json", "r") as file:
             data = json.load(file)
 
         world = None
